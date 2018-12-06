deaef61f53941760f7448abfe068367469bb1733
Keith Green <keithgreen@Keiths-MacBook-Pro.local>
2018-12-06 14:38:59 -0500

    added superuser; created fixtures dir inside main app; have Artist.json and Songs.json seed data inside fixtures; used loaddata to seed initial data

--- main/models.py
+++ main/models.py
@@ -5,9 +5,14 @@ class Artist(models.Model):
     photo_url = models.CharField(max_length=400, null=True, blank=True) #null is db related; blank is validation (forms)
     nationality = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Song(models.Model):
     title = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
     preview_url = models.CharField(max_length=400)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs') #Related name is what this model will be referred to in the foreign key model ie Artist
-    
+
+    def __str__(self):
+        return self.title
